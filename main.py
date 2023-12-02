8751e4f7985e785623e472e72b91fa6c030f267c
RemiSaurel <r.saurel5@gmail.com>
2023-12-02 11:05:57 +0100

    add: main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+from opti import *
+
+def printModulo(numero):
+    for i in range(3, 8):
+        print("Modulo " + str(i) + " : " + str(numero % i))
+
+
+if __name__ == '__main__':
+    # METTRE LES NUMEROS ETUDIANTS ICI
+
+    # NUM_REMI = 12345678
+    # NUM_ADAM = 87654321
+
+    # FONCTION POUR AFFICHER VOS MODULOS FACILEMENT
+    # printModulo(NUM_REMI)
+    # printModulo(NUM_ADAM)
+
+    # MODULO 3 à 7 POUR RÉMI :
+    # ...............
+    # MODULO 3 à 7 POUR ADAM :
+    # ...............
+
+    # REMI
+    print("REMI : ")
+    modeleDecoupePapierRemi(255, [85, 80, 75, 70, 60], [15, 14, 13, 16, 14], 3)
+
+    print("=====================================")
+
+    # ADAM
+    print("ADAM")
+    modeleDecoupePapierAdam(283, [110, 92, 80, 74, 70, 65], [15, 12, 10, 9, 7, 3], 2)
